58bd331a83375347223403795908b7e4514677d3
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2024-09-13 17:51:48 +0200

    två till övningar

--- Program.cs
+++ Program.cs
@@ -4,16 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number: ");
-            int tal = int.Parse(Console.ReadLine());
-            if (tal >= 0)
-            {
-                Console.WriteLine("you'v enterd a positiv number");
-            }
-            else
-            {
-                Console.WriteLine("you'v enterd a negativ number");
-            }
+            //Övningar 1
+            //Console.Write("Enter a number: ");
+            //int tal = int.Parse(Console.ReadLine());
+            //if (tal >= 0)
+            //{
+            //    Console.WriteLine("you'v enterd a positiv number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("you'v enterd a negativ number");
+            //}
+
+            //Övningar 2
+            //Console.Write("Enter a number: ");
+            //int tal1 = int.Parse(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //int tal2 = int.Parse(Console.ReadLine());
+
+            //if (tal1 < tal2)
+            //{
+            //    Console.WriteLine("{tal1} is smaller then {tal2}");
+            //}
+            //else if(tal1 > tal2){
+            //    Console.WriteLine($"{tal1} is bigger then {tal2}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(":(");
+            //}
+
+            // Övningar 3
+            //Console.Write("Enter a number: ");
+            //double tal1 = Convert.ToDouble(Console.ReadLine());
+            //double tal2 = 0;
+            //if ( tal1 >0 )
+            //{
+            //Console.Write("Enter a number: ");
+            // tal2 = Convert.ToDouble(Console.ReadLine());
+            //}
+            //Console.WriteLine( $"{tal1}/{tal2} = {tal1/tal2}");
+
         }
     }
 }
