22bb4e2b4f76355080c2c979756ebea20cb4b427
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2024-09-13 12:28:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercises_Conditions_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int tal = int.Parse(Console.ReadLine());
+            if (tal >= 0)
+            {
+                Console.WriteLine("you'v enterd a positiv number");
+            }
+            else
+            {
+                Console.WriteLine("you'v enterd a negativ number");
+            }
+        }
+    }
+}
